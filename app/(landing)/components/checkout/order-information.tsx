fe478bae54881c5ae6699e8cba40243b30df8fef
Your Name <you@example.com>
2026-03-17 15:54:21 +0700

    update tugas 3

--- app/(landing)/components/checkout/order-information.tsx
+++ app/(landing)/components/checkout/order-information.tsx
@@ -1,34 +1,59 @@
-import CardWithHeader from "../ui/card-with-header";
+"use client";
 
-const OrderInformation = () => {
+import CardWithHeader from "../ui/card-with-header";
+import { CustomerInfo } from "@/app/hooks/use-cart-store";
+
+type TOrderInformation = {
+  formData: CustomerInfo;
+  setFormData: React.Dispatch<React.SetStateAction<CustomerInfo>>;
+};
+
+const OrderInformation = ({ formData, setFormData }: TOrderInformation) => {
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
     <CardWithHeader title="Order Information">
-      <div className="p-5 space-y-4">
-        <div className="input-group">
-          <label htmlFor="full_name" className="block text-sm font-medium text-gray-700 mb-2">Full Name</label>
-          <input 
-            type="text" 
-            placeholder="Type your full name" 
-            id="full_name"
-            className="w-full px-3 py-2 border border-gray-300 rounded text-sm focus:outline-none focus:ring-2 focus:ring-primary"
+      <div className="p-5 space-y-6">
+        <div className="flex flex-col gap-2">
+          <label htmlFor="customerName" className="font-semibold text-gray-700">Full Name</label>
+          <input
+            type="text"
+            placeholder="Type your full name"
+            id="customerName"
+            name="customerName"
+            value={formData.customerName}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-primary"
           />
         </div>
-        <div className="input-group">
-          <label htmlFor="wa_number" className="block text-sm font-medium text-gray-700 mb-2">Whatsapp Number</label>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="customerContact" className="font-semibold text-gray-700">Whatsapp Number</label>
           <input
             type="text"
             placeholder="Type your whatsapp number"
-            id="wa_number"
-            className="w-full px-3 py-2 border border-gray-300 rounded text-sm focus:outline-none focus:ring-2 focus:ring-primary"
+            id="customerContact"
+            name="customerContact"
+            value={formData.customerContact ?? ""}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-primary"
           />
         </div>
-        <div className="input-group">
-          <label htmlFor="shipping_address" className="block text-sm font-medium text-gray-700 mb-2">Shipping Address</label>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="customerAddress" className="font-semibold text-gray-700">Shipping Address</label>
           <textarea
             placeholder="Type your shipping address"
-            id="shipping_address"
-            rows={4}
-            className="w-full px-3 py-2 border border-gray-300 rounded text-sm focus:outline-none focus:ring-2 focus:ring-primary resize-none"
+            id="customerAddress"
+            name="customerAddress"
+            rows={5}
+            value={formData.customerAddress}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-primary resize-none"
           />
         </div>
       </div>
